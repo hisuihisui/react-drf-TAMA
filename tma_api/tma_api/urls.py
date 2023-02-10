a4b5a869182ee072353cff74e928315dbec80251
hisui <ruru.apple.1212@gmail.com>
2023-02-10 23:15:19 +0900

    jira→tma

--- tma_api/tma_api/urls.py
+++ tma_api/tma_api/urls.py
@@ -1,4 +1,4 @@
-"""jira_api URL Configuration
+"""tma_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     # JWTのトークンを返すパスの設定
     path("authen/", include("djoser.urls.jwt")),
